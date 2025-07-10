9c3d9e6de0979bdcf5d75c1bc70a24bd13cdddde
viniciusrbr <viniciusgoes159@gmail.com>
2025-07-10 16:08:52 -0300

    feat: add routeAdapter

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,30 +2,14 @@ import express from 'express'
 
 import { makeSignUpController } from '../factories/makeSignUpController'
 import { makeSignInController } from '../factories/SignInController'
+import { routeAdapter } from './adapters/routeAdapter'
 
 const app = express()
 
 app.use(express.json())
 
-app.post('/sign-up', async (req, res) => {
-  const signUpController = makeSignUpController()
-
-  const { statusCode, body } = await signUpController.handle({
-    body: req.body,
-  })
-
-  res.status(statusCode).json(body)
-})
-
-app.post('/sign-in', async (req, res) => {
-  const signInController = makeSignInController()
-
-  const { statusCode, body } = await signInController.handle({
-    body: req.body,
-  })
-
-  res.status(statusCode).json(body)
-})
+app.post('/sign-up', routeAdapter(makeSignUpController()))
+app.post('/sign-in', routeAdapter(makeSignInController()))
 
 app.listen(3001, () => {
   console.log('Server is running on port 3001')
